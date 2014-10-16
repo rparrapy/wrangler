4c4feb8148e32ba6368f729b6091822ea4e86797
Rodrigo Parra <rodpar07@gmail.com>
2014-10-16 15:41:32 -0300

    Add csv to geojson support

--- wrangler.py
+++ wrangler.py
@@ -11,14 +11,15 @@ import csv
 @click.option('--sector', default=0, help='Indice de la columna del sector.')
 @click.option('--y', default=1, help='Nombre de la columna dey.')
 @click.option('--x', default=2, help='Nombre de la columna dey.')
-@click.option('--output', default='output.csv', help='Ruta al archivo csv de salida.')
+@click.option('--output', default='output.json', help='Ruta al archivo csv de salida.')
 @click.option('--verbose', is_flag=False)
 def convert_csv(input, sector, y, x, output, verbose):
 
      # Read in raw data from csv
     rawData = csv.reader(open(input, 'rbU'), delimiter=';', dialect=csv.excel_tab)
-    resultWriter = csv.writer(open(output, 'wb'), delimiter=';', dialect='excel')
-     
+    #resultWriter = csv.writer(open(output, 'wb'), delimiter=';', dialect='excel')
+    result = []
+    header = []
     
     utm_cols = [sector, y, x]
 
@@ -42,20 +43,74 @@ def convert_csv(input, sector, y, x, output, verbose):
 
             try:
                 lat_long = utm.to_latlon(elem_x, elem_y, elem_number, elem_letter)
-                print [str(c) for c in lat_long]
+                #print [str(c) for c in lat_long]
             except utm.error.OutOfRangeError, e:
                 print "Coordenadas invalidas: %s %s %s" % (elem_sector, elem_y, elem_x)
                 continue
 
             content = [str(c) for c in lat_long] + content
+            result.append(content)
+
         else:
             content = ['latitude', 'longitude'] + content
+            header += content
         
         #print 'about to write row: %s' % content
-        resultWriter.writerow(content)
+        #resultWriter.writerow(content)
         cont += 1
 
     print 'Se convirtieron %s filas' % cont
+    csv_to_geojson(result, header, output)
+    # Hasta aca conversion a CSV
+
+
+def csv_to_geojson(rows, header, out_file):
+    # the template. where data from the csv will be formatted to geojson
+    template = \
+        ''' \
+        {   
+            "type" : "Feature",
+            "geometry" : {
+                "type" : "Point",
+                "coordinates" : ["%s","%s"]},
+                "properties" : {
+                    %s
+                }
+        },
+        '''
+     
+    # the head of the geojson file
+    output = \
+        ''' \
+    { "type" : "Feature Collection",
+      "features" : [
+        '''
+
+
+    for i, row in enumerate(rows):
+        latitud = row[0]
+        longitud = row[1]
+        properties = ""
+        for j in range(2, len(row)):
+            if header[j] and row[j]:
+                properties += '\t\t\t\t\t"%s":"%s",\n' % (header[j], row[j].replace('"', '\\"'))
+        properties = properties[5:-1]
+        output += template % (longitud, latitud, properties)
+
+
+
+    # the tail of the geojson file
+    output += \
+        ''' \
+        ]
+    }
+        '''
+
+    outFileHandle = open(out_file, "w")
+    outFileHandle.write(output)
+    outFileHandle.close()
+
+
 
 if __name__ == '__main__':
     convert_csv()
